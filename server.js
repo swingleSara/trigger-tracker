71ecabc2a45c02ae458d08acb045660e40082339
Sara Swingle <sara.swingle.dev@gmail.com>
2022-07-05 10:46:53 -0600

    Add heroku port and procfile

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.get("/api/:name", (request, response) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`The server is now running on ${PORT}.  Better go catch it!`);
 });
