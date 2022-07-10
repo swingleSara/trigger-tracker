8489faea26a327f64a7bd79aee496e65f6f2ba1b
Sara Swingle <sara.swingle.dev@gmail.com>
2022-07-10 08:50:32 -0600

    Update PORT in server.js documentation

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
-const PORT = process.env.PORT || 2121;
+const PORT = 2121;
 require("dotenv").config();
 
 let db,
@@ -85,6 +85,6 @@ app.delete("/deleteDrink", (request, response) => {
     .catch((error) => console.error(error));
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
